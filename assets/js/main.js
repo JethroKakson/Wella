332c1acaa131df603c74cba8c28be5caa5c1e59b
JethroKakson <jethrokakson@gmail.com>
2024-07-18 08:32:43 +0300

    making changes on the js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,29 @@
   "use strict";
 
   /**
+   * Hero Slider
+   */
+  var swiper = new Swiper(".sliderFeaturedPosts", {
+    spaceBetween: 0,
+    speed: 500,
+    centeredSlides: true,
+    loop: true,
+    slideToClickedSlide: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".custom-swiper-button-next",
+      prevEl: ".custom-swiper-button-prev",
+    },
+  });
+
+  /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
   function toggleScrolled() {
@@ -123,18 +146,18 @@
   /**
    * Initiate Pure Counter
    */
-  new PureCounter();
+  // new PureCounter();
 
   /**
    * Init swiper sliders
    */
-  function initSwiper() {
-    document.querySelectorAll('.swiper').forEach(function(swiper) {
-      let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
-      new Swiper(swiper, config);
-    });
-  }
-  window.addEventListener('load', initSwiper);
+  // function initSwiper() {
+  //   document.querySelectorAll('.swiper').forEach(function(swiper) {
+  //     let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
+  //     new Swiper(swiper, config);
+  //   });
+  // }
+  // window.addEventListener('load', initSwiper);
 
   /**
    * Init isotope layout and filters
@@ -220,25 +243,3 @@
 
 })();
 
-  /**
-   * Hero Slider
-   */
-  var swiper = new Swiper(".sliderFeaturedPosts", {
-    spaceBetween: 0,
-    speed: 500,
-    centeredSlides: true,
-    loop: true,
-    slideToClickedSlide: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".custom-swiper-button-next",
-      prevEl: ".custom-swiper-button-prev",
-    },
-  });
